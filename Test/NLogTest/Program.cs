0249ce471692596ec9cbfd97835f916439352d60
mihao0804 <153752843@qq.com>
2019-11-19 22:21:19 +0800

    ...

---
+++ Test/NLogTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using NLog.Fluent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+namespace NLogTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+{
+            NLog.Logger logger = LogManager.GetCurrentClassLogger();
+            logger.Debug("hello world!");
+        }
+    }
+}
