f29a95f0df04ddf588ff25004711eb1a1a3e6848
mihao0804 <153752843@qq.com>
2019-12-06 21:11:25 +0800

    ...

---
+++ Test/JsonOperationTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+namespace JsonOperationTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonFilePath = @"..\..\.\connection.json";
+            using (System.IO.StreamReader file = System.IO.File.OpenText(jsonFilePath))
+            {
+                using (JsonTextReader reader = new JsonTextReader(file))
+                {
+                    JObject o = (JObject)JToken.ReadFrom(reader);
+                    string url = o["url"].ToString();
+                    string user_id = o["user_id"].ToString();
+                    string password = o["password"].ToString();
+                }
+            }
+        }
+    }
+}
